4506e895967e0692268289c0a43e15fa7f0d52bb
海鹏 郭 <386348907@qq.com>
2020-09-26 16:30:08 +0800

    2

--- Program.cs
+++ Program.cs
@@ -5,8 +5,39 @@ namespace main
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Hello World!");
+        {
+ 
+
+            PanDa panDa = new PanDa("pan dah");
+            PanDa panDa2 = new PanDa("pan dah2");
+
+            Console.WriteLine(panDa.name);
+            Console.WriteLine(panDa2.name);
+
+            Console.WriteLine(PanDa.allCount);
+            Console.WriteLine(100_000_000);
+
+            Console.Read();
+
         }
     }
+    public class PanDa {
+
+        public string name { get; set; }
+
+        public static int allCount { get; set; }
+
+
+        public PanDa(string _name) {
+
+            name = _name;
+            allCount++;
+
+
+        }
+
+
+    }
+
+
 }
